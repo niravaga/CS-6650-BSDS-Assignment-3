7b9d4c56860b4b15ad69d26d098138f3a34319e8
Nirav Aga <Nirav Aga>
2016-12-11 22:50:11 -0800

    Subscribers first working draft

--- KafkaEnterpriseApp/KafkaEnterpriseApp-web/src/main/java/restfulAPI/KafkaMessageResource.java
+++ KafkaEnterpriseApp/KafkaEnterpriseApp-web/src/main/java/restfulAPI/KafkaMessageResource.java
@@ -11,13 +11,12 @@ import com.amazonaws.services.dynamodbv2.document.DynamoDB;
 import com.amazonaws.services.dynamodbv2.document.Item;
 import com.amazonaws.services.dynamodbv2.document.Table;
 import com.amazonaws.services.dynamodbv2.model.AttributeValue;
-import com.amazonaws.services.dynamodbv2.model.GetItemResult;
 import com.amazonaws.services.dynamodbv2.model.PutItemRequest;
 import com.amazonaws.services.dynamodbv2.model.PutItemResult;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
-import javax.ejb.EJB;
 import javax.ejb.Stateless;
 
 import javax.ws.rs.Path;
@@ -27,10 +26,14 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerRecord;
-import static restfulAPI.KafkaPublisherResource.dynamoDB;
+import org.apache.kafka.common.TopicPartition;
+import static restfulAPI.KafkaSubscriberResource.dynamoDB;
 
 @Stateless
 @Path("/message")
@@ -65,14 +68,11 @@ public class KafkaMessageResource {
 
         System.out.println("Sending bean messages");
         try (Producer<String, String> producer = new KafkaProducer<String, String>(props)) {
-//            for (int i = 0; i < 100; i++) {
-////                producer.send(new ProducerRecord<>("Sports", Integer.toString(i), Integer.toString(i)));
-//            }
 
             String topic = getPublisherTopic(publisherId);
 
             if (topic != null && !topic.isEmpty()) {
-                ProducerRecord<String, String> record = new ProducerRecord<>(getPublisherTopic(publisherId), message);
+                ProducerRecord<String, String> record = new ProducerRecord<>(topic, message);
 
                 producer.send(record);
             } else {
@@ -111,7 +111,7 @@ public class KafkaMessageResource {
 
             if (item != null) {
                 String topic = item.asMap().get("topic").toString();
-//                System.out.println("Topic: " + topic);
+                System.out.println("Topic: " + topic);
                 return topic;
             }
         } catch (AmazonServiceException ase) {
@@ -132,4 +132,136 @@ public class KafkaMessageResource {
         return null;
     }
 
+    @GET
+    @Consumes(MediaType.TEXT_PLAIN)
+    @Produces(MediaType.TEXT_PLAIN)
+    public String getMessage(@QueryParam("subscriberId") String subscriberId) {
+        Map<String, Object> subscriberDetails = getSubscriberDetails(subscriberId);
+
+        String subscribedTopic = (String) subscriberDetails.get("topic");
+
+        System.out.println("Topic: " + subscribedTopic);
+        long offset = Long.parseLong((String) subscriberDetails.get("offset"));
+
+        System.out.println("Offset:" + offset);
+
+        Properties props = new Properties();
+        props.put("bootstrap.servers", "localhost:9092");
+        props.put("group.id", subscriberId);
+        props.put("enable.auto.commit", "true");
+        props.put("auto.commit.interval.ms", "1000");
+        props.put("max.poll.records", "1");
+        props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);
+        TopicPartition partition = new TopicPartition(subscribedTopic, 0);
+//        consumer.subscribe(Arrays.asList(subscribedTopic));
+        consumer.assign(Arrays.asList(partition));
+//        consumer.seekToBeginning(Arrays.asList(partition));
+
+        if (offset == 0) {
+//            consumer.seek(partition, offset);
+            consumer.seekToBeginning(Arrays.asList(partition));
+        }
+
+        System.out.println("Position: " + consumer.position(partition));
+        ConsumerRecords<String, String> records = consumer.poll(1000);
+
+        System.out.println("Polled");
+        String message = "";
+
+        System.out.println("NumRecords: " + records.count());
+        for (ConsumerRecord<String, String> record : records) {
+            message += record.value();
+        }
+
+        if (offset == 0) {
+            updateSuscriberDetails(subscriberId, subscribedTopic, "1");
+        }
+        System.out.println("Message:" + message);
+        consumer.commitSync();
+        consumer.close();
+        return message;
+    }
+
+    private Map<String, Object> getSubscriberDetails(String subscriberId) {
+        System.out.println("Getting subscriber topic for id: " + subscriberId);
+
+        AWSCredentials credentials = null;
+
+        try {
+            credentials = new ProfileCredentialsProvider("default").getCredentials();
+        } catch (Exception e) {
+            throw new AmazonClientException(
+                    "Cannot load the credentials from the credential profiles file. "
+                    + "Please make sure that your credentials file is at the correct "
+                    + "location (C:\\Users\\nirav\\.aws\\credentials), and is in valid format.",
+                    e);
+        }
+        dynamoDB = new AmazonDynamoDBClient(credentials);
+        Region usWest2 = Region.getRegion(Regions.US_WEST_2);
+        dynamoDB.setRegion(usWest2);
+
+        String tableName = "subscriber";
+        try {
+//            Map<String, AttributeValue> key = new HashMap<>();
+            DynamoDB db = new DynamoDB(dynamoDB);
+            Table table = db.getTable(tableName);
+            Item item = table.getItem("id", subscriberId);
+            System.out.println("Item " + item);
+
+            if (item != null) {
+                return item.asMap();
+            }
+        } catch (AmazonServiceException ase) {
+            System.out.println("Caught an AmazonServiceException, which means your request made it "
+                    + "to AWS, but was rejected with an error response for some reason.");
+            System.out.println("Error Message:    " + ase.getMessage());
+            System.out.println("HTTP Status Code: " + ase.getStatusCode());
+            System.out.println("AWS Error Code:   " + ase.getErrorCode());
+            System.out.println("Error Type:       " + ase.getErrorType());
+            System.out.println("Request ID:       " + ase.getRequestId());
+        } catch (AmazonClientException ace) {
+            System.out.println("Caught an AmazonClientException, which means the client encountered "
+                    + "a serious internal problem while trying to communicate with AWS, "
+                    + "such as not being able to access the network.");
+            System.out.println("Error Message: " + ace.getMessage());
+        }
+
+        return null;
+    }
+
+    private void updateSuscriberDetails(String subscriberId, String topic, String offset) {
+
+        String tableName = "subscriber";
+        try {
+//            Map<String, AttributeValue> key = new HashMap<>();
+            System.out.println("Updating id:" + subscriberId + "to offset:" + offset);
+            Map<String, AttributeValue> item = newItem(subscriberId, topic, offset);
+            PutItemRequest putItemRequest = new PutItemRequest(tableName, item);
+            PutItemResult putItemResult = dynamoDB.putItem(putItemRequest);
+
+        } catch (AmazonServiceException ase) {
+            System.out.println("Caught an AmazonServiceException, which means your request made it "
+                    + "to AWS, but was rejected with an error response for some reason.");
+            System.out.println("Error Message:    " + ase.getMessage());
+            System.out.println("HTTP Status Code: " + ase.getStatusCode());
+            System.out.println("AWS Error Code:   " + ase.getErrorCode());
+            System.out.println("Error Type:       " + ase.getErrorType());
+            System.out.println("Request ID:       " + ase.getRequestId());
+        } catch (AmazonClientException ace) {
+            System.out.println("Caught an AmazonClientException, which means the client encountered "
+                    + "a serious internal problem while trying to communicate with AWS, "
+                    + "such as not being able to access the network.");
+            System.out.println("Error Message: " + ace.getMessage());
+        }
+    }
+
+    private Map<String, AttributeValue> newItem(String id, String topic, String offset) {
+        Map<String, AttributeValue> item = new HashMap<>();
+        item.put("id", new AttributeValue(id));
+        item.put("topic", new AttributeValue(topic));
+        item.put("offset", new AttributeValue(offset));
+        return item;
+    }
 }
